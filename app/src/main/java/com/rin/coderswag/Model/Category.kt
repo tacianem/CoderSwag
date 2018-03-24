d8d094e4e3edb78921bb07474edbe765789c270d
tacianem <tacianeemartimiano@gmail.com>
2018-03-24 20:50:24 -0300

    Add base code

---
+++ app/src/main/java/com/rin/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.rin.coderswag.Model
+
+class Category (val title: String, val image: String)
---
+++ app/src/main/java/com/rin/coderswag/Services/DataService.kt
@@ -0,0 +1,7 @@
+package com.rin.coderswag.Services
+
+import com.rin.coderswag.Model.Category
+
+object DataService {
+
+}
